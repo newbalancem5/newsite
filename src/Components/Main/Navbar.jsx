77f2ffaed220dc0f762b34d25582c114808ff845
newbalancem5 <newbalancem5@gmail.com>
2019-06-19 17:56:32 +0300

    Update Navbar.jsx

--- src/Components/Main/Navbar.jsx
+++ src/Components/Main/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
-// import AboutCompany from '../NavBarElements/AboutCompany';
 
 class NavigationBar extends Component {
     render(){
@@ -14,12 +13,6 @@ class NavigationBar extends Component {
               <Nav.Link><Link to="/AboutCompany">О компании</Link></Nav.Link>
               <Nav.Link > <Link to="/PhotoGalery" > Фото Галерея</Link></Nav.Link>
             </Nav>
-            {/* <Router> 
-              <Switch>
-                <Route to="/" component={AboutCompany}/>
-
-              </Switch>
-            </Router> */}
           </Navbar>
 
 
