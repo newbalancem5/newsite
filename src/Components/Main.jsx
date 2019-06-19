37b32d257eb8e083f2da588dd0205fffda3cdd06
newbalancem5 <newbalancem5@gmail.com>
2019-06-19 17:56:25 +0300

    Update Main.jsx

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ElementsInfo from '../Components/Main/Elementsinfo';
 import Ads from '../Components/Main/Ads';
 import Form from '../Components/Main/FirstForm/FirstForm'
+import Logo from './Main/LogoCarousel/LogoCarousel'
 
 class Main extends Component {
     render(){
@@ -13,6 +14,9 @@ class Main extends Component {
                 </div>
                 <div className="container">
                     <Ads/>
+                </div>
+                <div className="container">
+                    <Logo/>
                 </div>
                 <div className="container">
                     <Form />
