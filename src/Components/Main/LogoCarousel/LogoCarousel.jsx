e8a729d95a61fe1a1584c9e4b38cab6bf8379cf2
newbalancem5 <newbalancem5@gmail.com>
2019-06-21 17:28:44 +0300

    Update LogoCarousel.jsx

--- src/Components/Main/LogoCarousel/LogoCarousel.jsx
+++ src/Components/Main/LogoCarousel/LogoCarousel.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
-import Logotip from "../Logo/isuzu.png"
-import Daf from '../Logo/daf.png'
-import Hino from '../Logo/Hino.png'
-// import Hyndai from '../Logo/Hyundai.gif'
-import Iveco from '../Logo/iveco.png'
+import Logotip from "../Logo/isuzus.png"
+import Daf from '../Logo/dafs.png'
+import Hino from '../Logo/Hinos.png'
+import Hynday from '../Logo/Hyunday.png'
+import Iveco from '../Logo/ivss.png'
+import Merin from '../Logo/Mercedess.png'
 
 
 const Logo = () => {
@@ -19,11 +20,13 @@ const Logo = () => {
     mouseDragEnabled={true}
     disableAutoPlayOnAction={true}
      >
-      <img width="50%" height="50%" src={Logotip} onDragStart={handleOnDragStart}  />
-      <img width="50%" height="50%" src={Daf} onDragStart={handleOnDragStart} className="yours-custom-class" />
-      <img width="50%" height="50%" src={Hino} onDragStart={handleOnDragStart} className="yours-custom-class" />
-      <img width="50%" height="50%" src={Logotip} onDragStart={handleOnDragStart} className="yours-custom-class" />
-      <img width="50%" height="50%" src={Iveco} onDragStart={handleOnDragStart} className="yours-custom-class" />
+      <img  src={Logotip} onDragStart={handleOnDragStart}  />
+      <img  src={Daf} onDragStart={handleOnDragStart} className="yours-custom-class" />
+      <img  src={Hino} onDragStart={handleOnDragStart} className="yours-custom-class" />
+      <img  src={Logotip} onDragStart={handleOnDragStart} className="yours-custom-class" />
+      <img  src={Iveco} onDragStart={handleOnDragStart} className="yours-custom-class" />
+      <img  src={Hynday} onDragStart={handleOnDragStart} className="yours-custom-class" />
+      <img  src={Merin} onDragStart={handleOnDragStart} className="yours-custom-class" />
     </AliceCarousel>
     </div>
   )
