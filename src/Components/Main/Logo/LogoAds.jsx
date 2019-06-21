08d82d20956e05fdc262ae3cad0c76a06062bff9
newbalancem5 <newbalancem5@gmail.com>
2019-06-21 17:28:53 +0300

    Update LogoAds.jsx

--- src/Components/Main/Logo/LogoAds.jsx
+++ src/Components/Main/Logo/LogoAds.jsx
@@ -1,35 +1,46 @@
 import React from 'react'
 import {Container, Col, Row, Image} from 'react-bootstrap'
-import Logotip from "../Logo/isuzu.png"
-import Daf from '../Logo/daf.png'
-import Hino from '../Logo/Hino.png'
-// import Hyndai from '../Logo/Hyundai.gif'
-import Iveco from '../Logo/iveco.png'
+import Logotip from "../Logo/isuzus.png"
+import Daf from '../Logo/daf.svg'
+import Hino from '../Logo/Hinos.png'
+import Hynday from '../Logo/Hyunday.png'
+import Iveco from '../Logo/ivecos.svg'
+import Merin from '../Logo/Mercedess.png'
 
 const LogoAds = () => {
     return(
         <div className="container">
             <Container>
                 <Row>
-                    <Col xs={6} md={4}>
+                    <Col xs={3} >
                         <Image
                         width="100%"
                         src={Logotip} />
                     </Col>
-                    <Col xs={6} md={4}>
+                    <Col xs={3}>
                         <Image
                         width="100%"
                         src={Daf}  />
                     </Col>
-                    <Col xs={6} md={4}>
+                    <Col xs={3}>
                         <Image
                         width="100%"
                         src={Hino}  />
                     </Col>
-                    <Col xs={6} md={4}>
+                    <Col xs={3}>
                         <Image
                         width="100%"
                         src={Iveco}  />
+                    </Col>
+                    <Col xs={3}>
+                    <Image
+                    width="100%"
+                    src={Merin} />
+                    </Col>
+                    <Col xs={3}>
+                    <Image
+                    width="100%"
+                    src={Hynday} />
                     </Col>
                 </Row>
             </Container>
