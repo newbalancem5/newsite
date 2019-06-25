a8482fe0c2cc6b7fab21332967b8e630f03a7562
newbalancem5 <newbalancem5@gmail.com>
2019-06-25 18:34:22 +0300

    Update Navbar.jsx

--- src/Components/Main/Navbar.jsx
+++ src/Components/Main/Navbar.jsx
@@ -1,18 +1,19 @@
 import React, {Component} from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
-
+import './Main.css'
 class NavigationBar extends Component {
     render(){
         return(
-            <Navbar bg="dark" variant="dark">            
-            <Navbar.Brand>
+            <Navbar bg="transparent" sticky="top" inline>            
+            <Navbar.Brand inline>
               <Link to="/">RefCar</Link>
               </Navbar.Brand>
-            <Nav className="mr-auto">
+            <Nav className="left">
               <Nav.Link><Link to="/AboutCompany">О компании</Link></Nav.Link>
               <Nav.Link > <Link to="/PhotoGalery" > Фото Галерея</Link></Nav.Link>
             </Nav>
+            
           </Navbar>
 
 
