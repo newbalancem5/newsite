7aa783f09ff3795a54d9da32467675a10537412d
newbalancem5 <newbalancem5@gmail.com>
2019-06-26 23:54:36 +0300

    Update Price.jsx

--- src/Components/NavBarElements/Price/Price.jsx
+++ src/Components/NavBarElements/Price/Price.jsx
@@ -1,11 +1,32 @@
 import React , {Component} from 'react';
-
+import Price1 from './PDF/trailerrepairPRISE.pdf'
+import {Col , Row, ListGroup} from 'react-bootstrap'
+import Footers from '../../Main/footers';
 
 
 class Price extends Component {
     render(){
         return(
-            <p>Price</p>
+            <div>
+            <div className="container">
+                <div className="container">
+                    <h1 className="title">Прайс компании</h1>
+                </div>
+                <div className="container">
+                    <Row>
+                        <Col>
+                        
+                        <ListGroup variant="flush">
+                            <ListGroup.Item><a href = {Price1} target="_blank">Ремонт прицепов</a></ListGroup.Item>
+                            <ListGroup.Item>Прайс№2</ListGroup.Item>
+                           
+                        </ListGroup>
+                        </Col>
+                    </Row>
+                </div>
+            </div>            
+            <Footers/>  
+            </div>
         )
     }
 } 
