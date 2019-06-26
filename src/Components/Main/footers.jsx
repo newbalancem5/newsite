dac72585f4da03e2a86298b2d6d4e26bf9aa316a
newbalancem5 <newbalancem5@gmail.com>
2019-06-26 23:54:45 +0300

    Update footers.jsx

--- src/Components/Main/footers.jsx
+++ src/Components/Main/footers.jsx
@@ -1,81 +1,82 @@
 import React from 'react';
 import './Main.css'
+import {FaFacebook} from 'react-icons/fa'
+import {FaVk} from 'react-icons/fa'
+import YandexLogo from './Logo/ya.png'
+import GisLogo from './Logo/2gis.png'
 
 
 const Footers = () => (
-    <footer class="footer">
-      <footer>
-                <div class="container-fluid text-center text-md-left" className="footer">
-
-
-<div class="row">
-
-  
-  <div class="col-md-6 mt-md-0 mt-3">
-
-    
-    <h5 class="text-uppercase">Иконки</h5>
-    <p>Иконки</p>
-
+  <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+    <div class="container">
+          <div class="row row-30">
+            <div class="col-md-4 col-xl-5">
+              <div class="pr-xl-4">
+                
+                <p>Сервисный центр «Рефкар» это широкий выбор услуг от прохождения сервисного ТО грузового автомобиля и установки  дополнительного оборудования, до ремонта и холодильного оборудования и систем управления автомобилем.</p>
+                <br/>
+                <a href="https://www.facebook.com/RefCar.Russia"><FaFacebook  className="fa"/ ></a>
+                <a href=" https://vk.com/refcar_krasnodar"><FaVk className="fa" /></a>
+                <a href="https://yandex.ru/maps/org/1357447726"> <img src={GisLogo} className="fa" alt=""/>  </a>
+                <a href="https://2gis.ru/krasnodar/firm/70000001018796423?fa821dba_ipp_key=v1561570610517%2Fv3394bdbf9a433ef5952ea5163aeca6afa04ab3%2F7oB7ygEPTeMY4gT%2Fjdgy2A%3D%3D&fa821dba_ipp_uid=1561570610516%2F8YU4OLABFFhQrP8I%2F9dsib5pW7evM8IQQYZoaYg%3D%3D&fa821dba_ipp_uid2=8YU4OLABFFhQrP8I%2F9dsib5pW7evM8IQQYZoaYg%3D%3D&fa821dba_ipp_uid1=1561570610516"> <img src={YandexLogo} className="fa" alt=""/> </a>
+              </div>
+            </div>
+            <div class="col-md-4">
+              {/* <h5>Контакты</h5>
+              <dl className="contact-list">
+                <dt>ИНН</dt>
+                <dd>2312226769</dd>
+              </dl>
+              <dl class="contact-list">
+                <dt>Адрес:</dt>
+                <dd>Большевистская 324</dd>
+              </dl>
+              <dl class="contact-list">
+                <dt>Электроная почта:</dt>
+                <dd><a href="mailto:#">info@refcar.ru</a></dd>
+              </dl>
+              <dl class="contact-list">
+                <dt>Телефон:</dt>
+                <dd>+7 991 357-49-84                   
+                  <br/>
+                  <dd>8 (800) 234-78-58  </dd>
+                </dd>
+              </dl> */}
+            </div>
+            <div class="col-md-4 col-xl-3">
+              <h5>Контакты</h5>
+              <dl className="contact-list">
+                {/* <dt>ИНН</dt> */}
+                <dd>2312226769</dd>
+              </dl>
+              <dl class="contact-list">
+                {/* <dt>Адрес:</dt> */}
+                <dd>Большевистская 324</dd>
+              </dl>
+              <dl class="contact-list">
+                {/* <dt>Электроная почта:</dt> */}
+                <dd><a href="mailto:#">info@refcar.ru</a></dd>
+              </dl>
+              <dl class="contact-list">
+                {/* <dt>Телефон:</dt> */}
+                <dd>+7 991 357-49-84                   
+                  <br/>
+                  <dd>8 (800) 234-78-58  </dd>
+                </dd>
+              </dl>
+            </div>
+          </div>
+        </div>
+        <div class="row no-gutters social-container">
+          <div class="col"></div>
+          <div class="col"></div>
+          <div class="col"></div>
+          <div class="col"></div>
+        </div>
+  <div class="container text-center">
+    <small>Copyright &copy; Your Website</small>
   </div>
-  
-
-  <hr class="clearfix w-100 d-md-none pb-3"/>
-
-  
-  <div class="col-md-3 mb-md-0 mb-3">
-
-    
-    <h5 class="text-uppercase">О компании</h5>
-
-    <ul class="list-unstyled">
-      <li>
-        <a href="#!">О компании</a>
-      </li>
-      <li>
-        <a href="#!">Наши приемущества</a>
-      </li>
-      <li>
-        <a href="#!">Связаться с нами</a>
-      </li>
-      <li>
-        <a href="#!">Контакты</a>
-      </li>
-      <li>
-        <a href="#!">Фотогалерея</a>
-      </li>
-    </ul>
-
-  </div>
-  <div class="col-md-3 mb-md-0 mb-3">
-    <h5 class="text-uppercase">Контактные данные</h5>
-
-    <ul class="list-unstyled">
-      <li>
-        <a href="#!">ООО «Рефкар»</a>
-      </li>
-      <li>
-        <a href="#!">ИНН 2312226769</a>
-      </li>
-      <li>
-        <a href="#!">2015 - 2019 гг</a>
-      </li>
-      <li>
-        <a href="#!">Большевистская 324 <br/>
-info@refcar.ru
-</a>
-      </li>
-      <li>
-      8 (800) 234-78-58 <br/>
-ПН-ПТ с 09:00 до 18:00 <br/>
-СБ-ВС - выходные<br/>
-      </li>
-    </ul>
-  </div>
-</div>
-</div>
-            </footer>
-    </footer>
+</footer>
   );
 
 
