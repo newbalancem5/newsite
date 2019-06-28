16aa9953eed6a50ba4cff7c55ca64cef9d787d7a
newbalancem5 <newbalancem5@gmail.com>
2019-06-28 22:39:42 +0300

    Update LogoAds.jsx

--- src/Components/Main/Logo/LogoAds.jsx
+++ src/Components/Main/Logo/LogoAds.jsx
@@ -3,17 +3,22 @@ import {Container, Col, Row, Image} from 'react-bootstrap'
 import Isuzu from './logo/isuzu.svg'
 import Daf from './logo/daf(2).svg'
 import Hino from './logo/hino.svg'
-import Iveco from './logo/iveco-1.svg'
+import Iveco from './logo/iveco.svg'
 import Mer from './logo/mercedes-benz.svg'
 import Hynday from './logo/hyundai.svg'
 import Scania from './logo/scania-2.svg'
 import Man from './logo/man-2.svg'
-import Shmidt from './logo/schmidt-1.svg'
-import Krone from './logo/krone.svg'
+import Shmidt from './logo/schmitz.svg'
+import Krone from './logo/krones.svg'
+import Koegel from './logo/Koegel.svg'
+import Wabco from './logo/wabco.svg'
+import Ashok from './logo/ashok.svg'
+import Saf from './logo/saf.svg'
+import './logo.css'
 
 const LogoAds = () => {
     return(
-        <div className="container">
+        <div className="container" >
             <Container>
                 <div className="container">
                 <Row>
@@ -68,6 +73,9 @@ const LogoAds = () => {
             <div className="container">
                 <Row>
                 <Col xs={3}>
+                    <Image
+                    width="50%"
+                    src={Wabco} />
                 </Col>
                 <Col xs={3}>
                     <Image
@@ -78,6 +86,36 @@ const LogoAds = () => {
                     <Image
                     width="50%"
                     src={Krone} />
+                    </Col>
+                    <Col>
+                    <Image
+                    width="70%"
+                    src={Ashok}
+                    />
+                    </Col>
+                </Row>
+            </div>
+            <div className="container">
+                <Row>
+                <Col xs={3}>
+                    
+                </Col>
+                <Col xs={3}>
+                <Image
+                    width="70%"
+                    
+                    src={Koegel} />
+                   
+                    </Col>
+                    <Col xs={3}>
+                    <Image
+                    width="70%"
+                    className="Saf"
+                    src={Saf} />
+                    </Col>
+                    <Col>
+                   
+                    
                     </Col>
                 </Row>
             </div>
