a09776f75a8537867c337a8cc30501de1840e9af
newbalancem5 <newbalancem5@gmail.com>
2019-06-28 22:40:37 +0300

    up

--- src/Components/Main/footers.jsx
+++ src/Components/Main/footers.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './Main.css'
 import {FaFacebook} from 'react-icons/fa'
 import {FaVk} from 'react-icons/fa'
-import YandexLogo from './Logo/ya.png'
-import GisLogo from './Logo/2gis.png'
+import YandexLogo from './Logo/YandexLogo.png'
+import GisLogo from './Logo/2gis.svg'
 
 
 const Footers = () => (
@@ -12,13 +12,12 @@ const Footers = () => (
           <div class="row row-30">
             <div class="col-md-4 col-xl-5">
               <div class="pr-xl-4">
-                
                 <p>Сервисный центр «Рефкар» это широкий выбор услуг от прохождения сервисного ТО грузового автомобиля и установки  дополнительного оборудования, до ремонта и холодильного оборудования и систем управления автомобилем.</p>
                 <br/>
                 <a href="https://www.facebook.com/RefCar.Russia"><FaFacebook  className="fa"/ ></a>
                 <a href=" https://vk.com/refcar_krasnodar"><FaVk className="fa" /></a>
-                <a href="https://yandex.ru/maps/org/1357447726"> <img src={GisLogo} className="fa" alt=""/>  </a>
-                <a href="https://2gis.ru/krasnodar/firm/70000001018796423?fa821dba_ipp_key=v1561570610517%2Fv3394bdbf9a433ef5952ea5163aeca6afa04ab3%2F7oB7ygEPTeMY4gT%2Fjdgy2A%3D%3D&fa821dba_ipp_uid=1561570610516%2F8YU4OLABFFhQrP8I%2F9dsib5pW7evM8IQQYZoaYg%3D%3D&fa821dba_ipp_uid2=8YU4OLABFFhQrP8I%2F9dsib5pW7evM8IQQYZoaYg%3D%3D&fa821dba_ipp_uid1=1561570610516"> <img src={YandexLogo} className="fa" alt=""/> </a>
+                <a href="https://yandex.ru/maps/org/1357447726"> <img src={YandexLogo} className="faya" alt=""/>  </a>
+                <a href="https://2gis.ru/krasnodar/firm/70000001018796423?fa821dba_ipp_key=v1561570610517%2Fv3394bdbf9a433ef5952ea5163aeca6afa04ab3%2F7oB7ygEPTeMY4gT%2Fjdgy2A%3D%3D&fa821dba_ipp_uid=1561570610516%2F8YU4OLABFFhQrP8I%2F9dsib5pW7evM8IQQYZoaYg%3D%3D&fa821dba_ipp_uid2=8YU4OLABFFhQrP8I%2F9dsib5pW7evM8IQQYZoaYg%3D%3D&fa821dba_ipp_uid1=1561570610516"> <img src={GisLogo} className="faya" alt=""/> </a>
               </div>
             </div>
             <div class="col-md-4">
@@ -59,9 +58,9 @@ const Footers = () => (
               </dl>
               <dl class="contact-list">
                 {/* <dt>Телефон:</dt> */}
-                <dd>+7 991 357-49-84                   
+                <dd>+7 900 271-54-33                   
                   <br/>
-                  <dd>8 (800) 234-78-58  </dd>
+                  {/* <dd>8 (800) 234-78-58  </dd> */}
                 </dd>
               </dl>
             </div>
