38286036ed0872f548f8462fa4b460490af9d104
newbalancem5 <newbalancem5@gmail.com>
2019-06-29 18:03:00 +0300

    Update Navbar.jsx

--- src/Components/Main/Navbar.jsx
+++ src/Components/Main/Navbar.jsx
@@ -5,15 +5,15 @@ import './Main.css'
 class NavigationBar extends Component {
     render(){
         return(
-            <Navbar bg="white" sticky="top" inline>            
+            <Navbar bg="" sticky="top" inline className="navbar">            
             <Navbar.Brand inline>
             
               </Navbar.Brand>
             <Nav className="left">
-            <Nav.Link ><Link to="/">Главная</Link></Nav.Link >
+            <Nav.Link ><Link to="/" className="navitem">Главная</Link></Nav.Link >
               {/* <Nav.Link><Link to="/AboutCompany">Контакты</Link></Nav.Link> */}
-              <Nav.Link > <Link to="/PhotoGalery" >Галерея</Link></Nav.Link>
-              <Nav.Link><Link to="/Price" > Стоимость работ </Link></Nav.Link>
+              <Nav.Link > <Link to="/PhotoGalery" className="navitem" >Галерея</Link></Nav.Link>
+              <Nav.Link><Link to="/Price" className="navitem"> Стоимость работ </Link></Nav.Link>
             </Nav>
             
           </Navbar>
