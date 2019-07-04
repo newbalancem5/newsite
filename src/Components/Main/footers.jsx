3e119ece407e1c1aaa22a90e00949ecc11697b5c
newbalancem5 <newbalancem5@gmail.com>
2019-07-04 18:13:10 +0300

    Update footers.jsx

--- src/Components/Main/footers.jsx
+++ src/Components/Main/footers.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './Main.css'
 import {FaFacebook} from 'react-icons/fa'
 import {FaVk} from 'react-icons/fa'
-import YandexLogo from './Logo/YandexLogo.png'
-import GisLogo from './Logo/2gis.svg'
+import {FaYandex} from 'react-icons/fa'
+import GisLogo from './Logo/gis.svg'
 
 
 const Footers = () => (
@@ -14,10 +14,10 @@ const Footers = () => (
               <div class="pr-xl-4">
                 <p>Сервисный центр «Рефкар» это широкий выбор услуг от прохождения сервисного ТО грузового автомобиля и установки  дополнительного оборудования, до ремонта и холодильного оборудования и систем управления автомобилем.</p>
                 <br/>
-                <a href="https://www.facebook.com/RefCar.Russia"><FaFacebook  className="fa"/ ></a>
-                <a href=" https://vk.com/refcar_krasnodar"><FaVk className="fa" /></a>
-                <a href="https://yandex.ru/maps/org/1357447726"> <img src={YandexLogo} className="faya" alt=""/>  </a>
-                <a href="https://2gis.ru/krasnodar/firm/70000001018796423?fa821dba_ipp_key=v1561570610517%2Fv3394bdbf9a433ef5952ea5163aeca6afa04ab3%2F7oB7ygEPTeMY4gT%2Fjdgy2A%3D%3D&fa821dba_ipp_uid=1561570610516%2F8YU4OLABFFhQrP8I%2F9dsib5pW7evM8IQQYZoaYg%3D%3D&fa821dba_ipp_uid2=8YU4OLABFFhQrP8I%2F9dsib5pW7evM8IQQYZoaYg%3D%3D&fa821dba_ipp_uid1=1561570610516"> <img src={GisLogo} className="faya" alt=""/> </a>
+                <a href="https://www.facebook.com/RefCar.Russia" className="fa"><FaFacebook  className="fa"/ ></a>
+                <a href=" https://vk.com/refcar_krasnodar" className="fa"><FaVk className="fa" /></a>
+                <a href="https://yandex.ru/maps/org/1357447726" className="faya"> <FaYandex className="faya" />  </a>
+                <a href="https://2gis.ru/krasnodar/firm/70000001018796423?fa821dba_ipp_key=v1561570610517%2Fv3394bdbf9a433ef5952ea5163aeca6afa04ab3%2F7oB7ygEPTeMY4gT%2Fjdgy2A%3D%3D&fa821dba_ipp_uid=1561570610516%2F8YU4OLABFFhQrP8I%2F9dsib5pW7evM8IQQYZoaYg%3D%3D&fa821dba_ipp_uid2=8YU4OLABFFhQrP8I%2F9dsib5pW7evM8IQQYZoaYg%3D%3D&fa821dba_ipp_uid1=1561570610516"  > <img src={GisLogo} alt="" className="icogisactive"/> </a>
               </div>
             </div>
             <div class="col-md-4">
@@ -29,13 +29,9 @@ const Footers = () => (
                 {/* <dt>ИНН</dt> */}
                 <dd>2312226769</dd>
               </dl>
-              <dl class="contact-list">
+              <dl class="adress">
                 {/* <dt>Адрес:</dt> */}
-                <dd>Большевистская 324</dd>
-              </dl>
-              <dl class="contact-list">
-                {/* <dt>Электроная почта:</dt> */}
-                <dd><a href="mailto:#">info@refcar.ru</a></dd>
+                <dd>г.Краснодар, ул.Большевистская 324</dd>
               </dl>
               <dl class="contact-list">
                 {/* <dt>Телефон:</dt> */}
@@ -43,6 +39,10 @@ const Footers = () => (
                   <br/>
                   {/* <dd>8 (800) 234-78-58  </dd> */}
                 </dd>
+              </dl>
+              <dl class="">
+                {/* <dt>Электроная почта:</dt> */}
+                <a className="linkactive" href="mailto:#">info@refcar.ru</a>
               </dl>
             </div>
           </div>
