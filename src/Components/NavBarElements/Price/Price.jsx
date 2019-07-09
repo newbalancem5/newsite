20c69ebbcdf95a209fba3b5b2a57e2adf19ee9d1
newbalancem5 <newbalancem5@gmail.com>
2019-07-09 23:09:07 +0300

    Update Price.jsx

--- src/Components/NavBarElements/Price/Price.jsx
+++ src/Components/NavBarElements/Price/Price.jsx
@@ -1,6 +1,6 @@
 import React , {Component} from 'react';
-import Price1 from './PDF/trailerrepairPRISE.pdf'
-import Price2 from './PDF/price2.pdf'
+// import Price1 from './PDF/trailerrepairPRISE.pdf'
+// import Price2 from './PDF/price2.pdf'
 import {Col , Row, ListGroup} from 'react-bootstrap'
 import Footers from '../../Main/footers'
 import './footer.css'
@@ -17,10 +17,9 @@ class Price extends Component {
                 <div className="container">
                     <Row>
                         <Col>
-                        
                         <ListGroup className="all" >
-                            <ListGroup.Item className="listgroup"><a  className="PriceItem">Прайсы находятся в разработке. Вся информация по номеру</a></ListGroup.Item>
-                            <ListGroup.Item className="listgroup"><a   className="PriceItem">8 900 271 54 33</a></ListGroup.Item>
+                            <ListGroup.Item className="listgroup"><a className="PriceItem">Прайсы находятся в разработке. Вся информация по номеру</a></ListGroup.Item>
+                            <ListGroup.Item className="listgroup"><a className="PriceItem">8 900 271 54 33</a></ListGroup.Item>
                             {/* <ListGroup.Item className="listgroup"><a href = {Price1} target="_blank"  className="PriceItem">Ремонт прицепов</a></ListGroup.Item>
                             <ListGroup.Item className="listgroup"><a href = {Price2} target="_blank" className="PriceItem" >Полуприцепы</a></ListGroup.Item> */}
                         </ListGroup>
