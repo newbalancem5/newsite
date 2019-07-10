575fb1d360ccd9df06f170b245f1f7daa2ff62a4
newbalancem5 <newbalancem5@gmail.com>
2019-07-10 23:07:02 +0300

    add mobile menu

--- src/Components/Main/Navbar.jsx
+++ src/Components/Main/Navbar.jsx
@@ -1,23 +1,28 @@
 import React, {Component} from 'react';
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import './Main.css'
 class NavigationBar extends Component {
     render(){
         return(
-            <Navbar bg="" sticky="top" inline className="navbar">            
-            <Navbar.Brand inline>
-            
-              </Navbar.Brand>
-            <Nav className="left">
-            <Nav.Link ><Link to="/" className="navitem">Главная</Link></Nav.Link >
-              {/* <Nav.Link><Link to="/AboutCompany">Контакты</Link></Nav.Link> */}
-              <Nav.Link > <Link to="/PhotoGalery" className="navitem" >Галерея</Link></Nav.Link>
+          <Navbar collapseOnSelect expand="lg" bg="" sticky="top" inline className="navbar">
+  <Navbar.Brand href="#home" className="Brand" >Refcar</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+    <Nav.Link ><Link to="/" className="navitem">Главная</Link></Nav.Link >
+    <Nav.Link > <Link to="/PhotoGalery" className="navitem"  >Галерея</Link></Nav.Link>
               <Nav.Link><Link to="/Price" className="navitem"> Стоимость работ </Link></Nav.Link>
-            </Nav>
-            
-          </Navbar>
-
+    </Nav>
+    <Nav>
+      <Nav.Link  className="navitem"> 8 (900) 271-54-33</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+      
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+                 
 
         );
     }
