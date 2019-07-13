3fedb1fa67895d8600d0ce1dfb8bedaa0b5cb50c
newbalancem5 <newbalancem5@gmail.com>
2019-07-13 13:07:00 +0300

    Update Navbar.jsx

--- src/Components/Main/Navbar.jsx
+++ src/Components/Main/Navbar.jsx
@@ -2,29 +2,28 @@ import React, {Component} from 'react';
 import {Navbar, Nav, Image } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import './Main.css'
-import Logo from './Logo.png'
+import Logo from './ref.svg'
+
 class NavigationBar extends Component {
     render(){
         return(
           <Navbar collapseOnSelect expand="lg" bg="" sticky="top" inline className="navbar">
-  <Navbar.Brand href="#home" className="Brand" ><Image src={Logo} width="100" /></Navbar.Brand>
+  <Navbar.Brand href="#home" className="Brand" ><Image src={Logo} width="120" /></Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto">
-    <Nav.Link ><Link to="/" className="navitem">Главная</Link></Nav.Link >
-    <Nav.Link > <Link to="/PhotoGalery" className="navitem"  >Галерея</Link></Nav.Link>
-              <Nav.Link><Link to="/Price" className="navitem"> Стоимость работ </Link></Nav.Link>
+    <Nav className="mr-auto itemsbrand">
+    <Nav.Link href="#"><Link to="/" className="navitem">Главная</Link></Nav.Link >
+    <Nav.Link href="#" > <Link to="/PhotoGalery" className="navitem"  >Галерея</Link></Nav.Link>
+              <Nav.Link href="#"><Link to="/Price" className="navitem"> Стоимость работ </Link></Nav.Link>
     </Nav>
     <Nav>
-      <Nav.Link  className="navitem"> 8 (900) 271-54-33</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
+      <Nav.Link href="tel:89002715433"  className=" phonebrand"> 8 (900) 271-54-33</Nav.Link>
+      <Nav.Link eventKey={1} >
       
       </Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
-                 
-
         );
     }
 }
