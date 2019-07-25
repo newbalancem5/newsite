90708127d4e845d065e5a7fa0c48a8ff1b8a834d
newbalancem5 <newbalancem5@gmail.com>
2019-07-25 17:44:08 +0300

    Update Navbar.jsx

--- src/Components/Main/Navbar.jsx
+++ src/Components/Main/Navbar.jsx
@@ -1,42 +1,30 @@
 import React, {Component} from 'react';
 import {Navbar, Nav, Image } from 'react-bootstrap';
-import {Link, Switch, Route} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import './Main.css'
 import Logo from './ref.svg'
-import Main from '../Main';
-import AboutCompany from '../NavBarElements/AboutCompany/AboutCompany';
-import PhotoGalery from '../NavBarElements/PhotoGalery/PhotoGalery';
-import Price from '../NavBarElements/Price/Price';
+
 
 class NavigationBar extends Component {
     render(){
         return(
-          <div>
+          
           <Navbar collapseOnSelect expand="lg" bg="" sticky="top" inline className="navbar">
   <Navbar.Brand href="#home" className="Brand" ><Link to="/"><Image src={Logo} width="120" /></Link></Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto itemsbrand">
-    <Nav.Link href="#"><Link to="/" className="navitem">Главная</Link></Nav.Link >
-    <Nav.Link href="#" > <Link to="/PhotoGalery" className="navitem"  >Галерея</Link></Nav.Link>
-              <Nav.Link href="#"><Link to="/Price" className="navitem"> Стоимость работ </Link></Nav.Link>
+    <Nav.Link href="/"><Link to="/" className="navitem">Главная</Link></Nav.Link >
+    <Nav.Link href="/PhotoGalery" > <Link to="/PhotoGalery" className="navitem"  >Галерея</Link></Nav.Link>
+              <Nav.Link href="/Price"><Link to="/Price" className="navitem"> Стоимость работ </Link></Nav.Link>
     </Nav>
     <Nav>
       <Nav.Link href="tel:89002715433"  className=" phonebrand"> 8 (900) 271-54-33</Nav.Link>
       <Nav.Link eventKey={1} >
-      
       </Nav.Link>
     </Nav>
   </Navbar.Collapse>
-  
 </Navbar>
-<Switch>
-<Route exact path="/" component={Main} />
-    <Route path="/AboutCompany" component={AboutCompany} />
-    <Route path="/PhotoGalery" component={PhotoGalery} />
-    <Route path="/Price" component={Price} />
-</Switch>
-</div>
 
         );
     }
