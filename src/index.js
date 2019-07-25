edeaa339a6bf17a2e72560c86c85509fa2bd8527
newbalancem5 <newbalancem5@gmail.com>
2019-07-25 17:44:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import { render } from 'react-dom'
-import  { BrowserRouter} from 'react-router-dom'
+import  { BrowserRouter , Switch, Route} from 'react-router-dom'
 import Navbar from './Components/Main/Navbar'
 // import Error from './Components/404'
 import './Components/NavBarElements/Price/footer.css'
+import Main from './Components/Main';
+import PhotoGalery from './Components/NavBarElements/PhotoGalery/PhotoGalery';
+import Price from './Components/NavBarElements/Price/Price';
+import Errors from './Components/404';
+import { createBrowserHistory } from "history";
 
 
-render((
-  <BrowserRouter>
-    <Navbar />
+const history = createBrowserHistory()
+render(
+  (
+  
+  <BrowserRouter history={history}>
+    <div>
+      <Navbar />
+      <Switch>
+    <Route exact path="/" component={Main} />
+    <Route path="/PhotoGalery" component={PhotoGalery} />
+    <Route path="/Price" component={Price} />
+    <Route path="*" component={Errors} />
+      </Switch>
+    </div>
 </BrowserRouter>
   ),
    document.getElementById('root'));
